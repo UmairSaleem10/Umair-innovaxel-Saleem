6cb97fa3da1b0bb67350fb7bee9a898b342d9db9
Umair Saleem <126170701+UmairSaleem10@users.noreply.github.com>
2025-05-03 02:58:21 -0700

    create/ post short-url

---
+++ URL Assessment/controllers/urlController.js
@@ -0,0 +1,32 @@
+const Url = require('../models/Url');
+const shortid = require('shortid');
+const validator = require('validator');
+
+// createShortUrl 
+exports.createShortUrl = async (req, res) => {
+  try {
+    const { url } = req.body;
+    
+    if (!url || !validator.isURL(url)) {
+      return res.status(400).json({ error: 'Invalid URL' }); // for error handle
+    }
+
+    let existingUrl = await Url.findOne({ originalUrl: url }); 
+    
+    if (existingUrl) {
+      return res.json(existingUrl);
+    }
+
+    const shortCode = shortid.generate();
+    const newUrl = new Url({
+      originalUrl: url,
+      shortCode
+    });
+
+    await newUrl.save();
+    res.status(201).json(newUrl);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
